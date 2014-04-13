20f6d4d7552e03d91c4a5fa21313e4fe71dbec66
Jonathan Geiger <geiger@freshbooks.com>
2014-04-13 16:46:20 -0400

    Rename to Ember.Responsive.Media.
    We don't want to clobber the namespace too much.

--- tests/media-test.js
+++ tests/media-test.js
@@ -1,7 +1,7 @@
-module('Ember.Media');
+module('Ember.Responsive.Media');
 
 test('matchers can be set at construction', function() {
-  var subject = Ember.Media.create({
+  var subject = Ember.Responsive.Media.create({
     matchers: { all: 'all' }
   });
 
@@ -9,14 +9,14 @@ test('matchers can be set at construction', function() {
 });
 
 test('matchers can be added dynamically', function() {
-  var subject = Ember.Media.create();
+  var subject = Ember.Responsive.Media.create();
   subject.match('all', 'not all');
 
   equal(false, subject.get('all.matches'));
 });
 
 test('matchers have a corresponding isser', function() {
-  var subject = Ember.Media.create();
+  var subject = Ember.Responsive.Media.create();
   subject.match('mobile', 'not all');
 
   equal(false, subject.get('isMobile'));
