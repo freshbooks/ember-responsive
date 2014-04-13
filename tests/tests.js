378ba19e410a858a901bd8ea45b62f73537f7c59
Jonathan Geiger <geiger@freshbooks.com>
2014-04-13 16:00:01 -0400

    Add the start of the library.

--- tests/tests.js
+++ tests/tests.js
@@ -3,3 +3,18 @@ module('test');
 test('Ember.Responsive exists', function() {
   ok(Ember.Responsive);
 });
+
+test('matchers can be set at construction', function() {
+  var subject = Ember.Responsive.create({
+    matchers: { all: 'all' }
+  });
+
+  equal(true, subject.get('all.matches'));
+});
+
+test('matchers can be added dynamically', function() {
+  var subject = Ember.Responsive.create();
+  subject.match('all', 'not all');
+
+  equal(false, subject.get('all.matches'));
+});
