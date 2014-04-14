738121647bdd25644144dfc9fccb71b1e07b1ac8
Jonathan Geiger <geiger@freshbooks.com>
2014-04-13 20:06:17 -0400

    Add tasks for building and testing the library.

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['lib/responsive.js', 'lib/*.js']
+        }
+      }
+    },
+
+    concat: {
+      dist: {
+        src: ['lib/responsive.js', 'lib/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      },
+      test: {
+        src: [
+          'bower_components/jquery/dist/jquery.js',
+          'bower_components/handlebars/handlebars.js',
+          'bower_components/ember/ember.js',
+          'bower_components/qunit/qunit/qunit.js',
+          'bower_components/sinonjs/sinon.js',
+          'dist/ember-responsive.js',
+          'tests/**.js',
+        ],
+        dest: 'dist/<%= pkg.name %>.test.js'
+      }
+    },
+
+    testem: {
+      test: {
+        options: {
+          test_page: 'tests/index.html',
+          framework: 'qunit',
+          launch_in_dev: ['PhantomJS', 'Chrome'],
+          launch_in_ci: ['PhantomJS', 'Chrome'],
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-testem');
+
+  grunt.registerTask('test', 'Run tests using testem and PhantomJS',
+                     ['concat:test', 'testem:ci:test']);
+
+  grunt.registerTask('dist', 'Create a distributable version of <%= pkg.nam %>',
+                     ['uglify:dist', 'concat:dist']);
+}
