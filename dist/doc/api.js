ac0ace3aed6d8041d9f4caccfb4edb607dce6791
Jonathan Geiger <geiger@freshbooks.com>
2014-04-14 19:43:31 -0400

    Release v0.1.2

---
+++ dist/doc/api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Ember.Application.initializer",
+        "Ember.Application.responsive",
+        "Ember.Responsive.Media"
+    ],
+    "modules": [
+        "ember-responsive"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember-responsive",
+            "name": "ember-responsive",
+            "description": "The main namespace for `Ember.Responsive`."
+        }
+    ]
+} };
+});
