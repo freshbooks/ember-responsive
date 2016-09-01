4d5fb174bad8f0c48829ec662a197de88e3de941
Danas Gutaravicius <danas@openg.lt>
2016-09-01 16:37:20 +0300

    add changeable media tags

--- app/initializers/responsive.js
+++ app/initializers/responsive.js
@@ -1,2 +1,14 @@
-import responsive from 'ember-responsive/initializers/responsive';
-export default responsive;
+import Ember from 'ember';
+import ENV from '../config/environment';
+
+export default {
+  name: 'responsive',
+  initialize: function(){
+    let application = arguments[1] || arguments[0];
+    let propertyName = Ember.get(ENV, 'responsive.propertyName') || 'media';
+    application.inject('controller', propertyName, `service:media`);
+    application.inject('component', propertyName, `service:media`);
+    application.inject('route', propertyName, `service:media`);
+    application.inject('view', propertyName, `service:media`);
+  }
+};
