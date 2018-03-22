621ecfbf2828373af4b4ea9247000690fa367c43
cibernox <miguel.camba@gmail.com>
2018-03-22 14:52:55 +0100

    Update to new testing API

--- config/ember-try.js
+++ config/ember-try.js
@@ -15,7 +15,8 @@ module.exports = function() {
           name: 'ember-lts-2.12',
           npm: {
             devDependencies: {
-              'ember-source': '~2.12.0'
+              'ember-source': '~2.12.0',
+              'ember-native-dom-event-dispatcher': '~0.6.4'
             }
           }
         },
@@ -23,7 +24,8 @@ module.exports = function() {
           name: 'ember-lts-2.16',
           npm: {
             devDependencies: {
-              'ember-source': '~2.16.0'
+              'ember-source': '~2.16.0',
+              'ember-native-dom-event-dispatcher': '~0.6.4'
             }
           }
         },
@@ -31,7 +33,8 @@ module.exports = function() {
           name: 'ember-lts-2.18',
           npm: {
             devDependencies: {
-              'ember-source': '~2.18.0'
+              'ember-source': '~2.18.0',
+              'ember-native-dom-event-dispatcher': '~0.6.4'
             }
           }
         },
--- tests/unit/services/media-test.js
+++ tests/unit/services/media-test.js
@@ -1,95 +1,95 @@
-import Ember from 'ember';
-import { moduleFor, test } from 'ember-qunit';
+import { classify } from '@ember/string';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
 const mediaRules = {
   mobile:  '(max-width: 767px)',
   jumbo:   '(min-width: 1201px)'
 };
 
-moduleFor('service:media', 'Unit | Service | media', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
-});
+module('Unit | Service | media', function(hooks) {
+  setupTest(hooks);
 
-test('it matches on init', function(assert) {
+  test('it matches on init', function(assert) {
+    const subject = this.owner.factoryFor('service:media').create({
+      breakpoints: mediaRules,
+      match: sinon.stub()
+    });
 
-  const subject = this.subject({
-    breakpoints: mediaRules,
-    match: sinon.stub()
+    assert.ok(subject.match.withArgs('mobile','(max-width: 767px)').calledOnce);
+    assert.ok(subject.match.withArgs('jumbo','(min-width: 1201px)').calledOnce);
   });
 
-  assert.ok(subject.match.withArgs('mobile','(max-width: 767px)').calledOnce);
-  assert.ok(subject.match.withArgs('jumbo','(min-width: 1201px)').calledOnce);
+  test('matchers can be added dynamically', function(assert) {
+    var subject = this.owner.factoryFor('service:media').create({ breakpoints: mediaRules });
+    subject.match('all', 'not all');
+
+    assert.equal(subject.get('all.matches'), false);
+  });
+
+  test('matchers have a corresponding isser', function(assert) {
+    var subject = this.owner.factoryFor('service:media').create({ breakpoints: mediaRules });
+    subject.match('mobile', 'not all');
+
+    assert.equal(subject.get('isMobile'), false);
+  });
+
+  test('matches property returns matching matchers', function(assert) {
+    var subject = this.owner.factoryFor('service:media').create({ breakpoints: mediaRules });
+    subject.match('mobile', 'all');
+    subject.match('all', 'all');
+    subject.match('none', 'not all');
+
+    assert.deepEqual(subject.get('matches').toArray(), ['mobile', 'all']);
+  });
+
+  test('classNames property returns matching matchers as classes', function(assert) {
+    var subject = this.owner.factoryFor('service:media').create({ breakpoints: mediaRules });
+    subject.match('mobileDevice', 'all');
+    subject.match('all', 'all');
+    subject.match('none', 'not all');
+
+    assert.equal(subject.get('classNames'), 'media-mobile-device media-all');
+  });
+
+  test('classNames is correctly bound to the matches property', function(assert) {
+    var subject = this.owner.factoryFor('service:media').create({ breakpoints: mediaRules });
+
+    subject.match('one', 'all');
+    assert.equal(subject.get('classNames'), 'media-one');
+
+    subject.match('two', 'all');
+    assert.equal(subject.get('classNames'), 'media-one media-two');
+
+    subject.match('one', 'none');
+    assert.equal(subject.get('classNames'), 'media-two');
+  });
+
+  test('matcher\'s isser property notifies upon change', function(assert) {
+    var listener, matcher, name = 'somethingUnique',
+      subject = this.owner.factoryFor('service:media').create({ breakpoints: mediaRules }),
+      observer = sinon.spy();
+
+    subject.addObserver('is'+classify(name), this, observer);
+    //First call
+    subject.match(name, 'query');
+
+    listener = subject.get('listeners')[name];
+
+    matcher = {}; // Dummy MediaQueryList
+    matcher.matches = true;
+    //Second call
+    listener(matcher);
+
+    matcher.matches = false;
+    //Third call
+    listener(matcher);
+
+    assert.equal(
+      observer.callCount,
+      3,
+      'Expected 3 calls to an observer, '+observer.callCount+' were called instead'
+    );
+  });
 });
 
-test('matchers can be added dynamically', function(assert) {
-  var subject = this.subject({ breakpoints: mediaRules });
-  subject.match('all', 'not all');
-
-  assert.equal(subject.get('all.matches'), false);
-});
-
-test('matchers have a corresponding isser', function(assert) {
-  var subject = this.subject({ breakpoints: mediaRules });
-  subject.match('mobile', 'not all');
-
-  assert.equal(subject.get('isMobile'), false);
-});
-
-test('matches property returns matching matchers', function(assert) {
-  var subject = this.subject({ breakpoints: mediaRules });
-  subject.match('mobile', 'all');
-  subject.match('all', 'all');
-  subject.match('none', 'not all');
-
-  assert.deepEqual(subject.get('matches').toArray(), ['mobile', 'all']);
-});
-
-test('classNames property returns matching matchers as classes', function(assert) {
-  var subject = this.subject({ breakpoints: mediaRules });
-  subject.match('mobileDevice', 'all');
-  subject.match('all', 'all');
-  subject.match('none', 'not all');
-
-  assert.equal(subject.get('classNames'), 'media-mobile-device media-all');
-});
-
-test('classNames is correctly bound to the matches property', function(assert) {
-  var subject = this.subject({ breakpoints: mediaRules });
-
-  subject.match('one', 'all');
-  assert.equal(subject.get('classNames'), 'media-one');
-
-  subject.match('two', 'all');
-  assert.equal(subject.get('classNames'), 'media-one media-two');
-
-  subject.match('one', 'none');
-  assert.equal(subject.get('classNames'), 'media-two');
-});
-
-test('matcher\'s isser property notifies upon change', function(assert) {
-  var listener, matcher, name = 'somethingUnique',
-    subject = this.subject({ breakpoints: mediaRules }),
-    observer = sinon.spy();
-
-  subject.addObserver('is'+Ember.String.classify(name), this, observer);
-  //First call
-  subject.match(name, 'query');
-
-  listener = subject.get('listeners')[name];
-
-  matcher = {}; // Dummy MediaQueryList
-  matcher.matches = true;
-  //Second call
-  listener(matcher);
-
-  matcher.matches = false;
-  //Third call
-  listener(matcher);
-
-  assert.equal(
-    observer.callCount,
-    3,
-    'Expected 3 calls to an observer, '+observer.callCount+' were called instead'
-  );
-});
