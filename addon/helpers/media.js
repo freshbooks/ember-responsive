8af75d9b17f3a9180590d1296112560969dbefa0
Julien Palmas <julien@crx.io>
2019-09-25 10:34:26 +0200

    🔥 remove unused `run` import

--- addon/helpers/media.js
+++ addon/helpers/media.js
@@ -1,7 +1,6 @@
 import Helper from '@ember/component/helper';
 import { inject as service } from '@ember/service';
 import { get } from '@ember/object';
-import { run } from '@ember/runloop';
 
 export default Helper.extend({
   init() {
