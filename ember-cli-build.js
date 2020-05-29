ef0ff0a541b1b195856928c25e6ba015188182aa
Scott Newcomer <snewcomer24@gmail.com>
2020-05-29 09:25:53 -0500

    bump travis and ember-try to blueprint

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,11 +4,11 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    vendorFiles: { 'jquery.js': null, 'app-shims.js': null }
+    // Add options here
   });
 
   /*
-    This build file specifes the options for the dummy test app of this
+    This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
