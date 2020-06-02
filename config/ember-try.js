b81d06845708c240e957b6be4bb3dac843683a24
Scott Newcomer <snewcomer24@gmail.com>
2020-06-01 20:39:12 -0500

    Only support 3.13

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,18 +7,10 @@ module.exports = async function() {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.8',
+        name: 'ember-lts-3.13',
         npm: {
           devDependencies: {
-            'ember-source': '~3.8.0'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-3.12',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.12.0'
+            'ember-source': '~3.13.0'
           }
         }
       },
